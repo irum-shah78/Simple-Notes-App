bf25c58960893d05f229c4b3083e44fee4a71e7b
irum-shah78 <irams4812@gmail.com>
2024-03-18 23:24:40 +0500

    Completed

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const notesContainer = document.querySelector('.notes-container');
 const createBtn = document.querySelector('.btn');
+
 let notes = document.querySelectorAll('.input-box');
 
-createBtn.addEventListener('click', () =>{
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem('notes');
+}
+
+function updateStorage() {
+  localStorage.setItem('notes', notesContainer.innerHTML);
+}
+showNotes();
+
+createBtn.addEventListener('click', () => {
   let inputBox = document.createElement('p');
   let img = document.createElement('img');
   inputBox.className = 'input-box';
@@ -11,8 +21,23 @@ createBtn.addEventListener('click', () =>{
   notesContainer.appendChild(inputBox).appendChild(img);
 })
 
-notesContainer.addEventListener('click', function(e){
-  if(e.target.tagName === 'IMG'){
+notesContainer.addEventListener('click', function (e) {
+  if (e.target.tagName === 'IMG') {
     e.target.parentElement.remove();
+    updateStorage();
+  } else if (e.target.tagName === 'P') {
+    notes = document.querySelectorAll('.input-box');
+    notes.forEach(nt => {
+      nt.onkeyup = function () {
+        updateStorage();
+      }
+    })
+  }
+})
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
+    document.execCommand('insertLineBreak');
+    event.preventDefault();
   }
 })
